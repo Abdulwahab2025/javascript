b28d26f7ae2df74b83b154213904f4038158d17b
Abdulwahab <muhamedzadani22@gmail.com>
2025-07-25 18:58:48 +0100

    arrays work

---
+++ arrays/script.js
@@ -0,0 +1,29 @@
+let ages = [23, 45]
+// console.log(firstNames[0])
+// console.log(firstNames[1])
+// console.log(ages[1])
+// console.log(ages.length)
+
+// let magacaKowaad = firstNames[1]
+// console.log(magacaKowaad)
+// console.log(firstNames[1])
+
+// firstNames.push("Abdul","Osman","Fatima")
+
+// let firstNames = ["John", "Ali", "Abdul", "Osman", "Fatima"];
+
+// firstNames.pop()
+// firstNames.unshift("Amira")
+// firstNames.shift()
+// console.log(firstNames)
+// firstNames.length = 2
+// console.log(firstNames)
+
+var students = [ 'Ayman', 'Hidaya', 'Bilal', 'Roodo', 'Ridwaan', 'Mohamed' ];
+var anotherSmallArray = students.slice(0, 3); // create a new array from students
+console.log(anotherSmallArray);
+// [ 'Ayman', 'Hidaya', 'Bilal' ]
+
+students.reverse()
+console.log(students)
+
---
+++ arrays/variables/script.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i <  100; i++) {
+
+// console.log("Abdulwahab", i);
+
+// }
+
+
+let i = 0
+while ( i < 100) {
+  console.log("Mehmet", i);
+  i++
+}
+
+
+
